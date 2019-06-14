849597ea8252c1838cfd323601d7488af3cddb3a
Houcem Eddine Malek Chta <HEM@INTG.Corp>
2019-06-14 17:42:16 +0100

    import portfolio

--- Application/src/app/pages/home/home.component.ts
+++ Application/src/app/pages/home/home.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component ,Input } from '@angular/core';
 
 @Component({
+  selector:'app-home',
   templateUrl: 'home.component.html',
   styleUrls: [ './home.component.scss' ]
 })
 
 export class HomeComponent {
+  @Input() childMessage: string;
   constructor() {}
 
   helloWorld() {
     alert('Hello world!');
+}
+
+getResolution() {
+  alert("Your screen resolution is: " + screen.width + "x" + screen.height);
 }
 }
